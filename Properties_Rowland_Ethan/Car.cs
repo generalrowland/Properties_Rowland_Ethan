720f5c111ffc9290d45e2814b0a38f9c426f7d80
generalrowland <ethandale0916@gmail.com>
2023-04-18 13:18:47 -0500

    s

---
+++ Properties_Rowland_Ethan/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Properties_Rowland_Ethan
+{
+    class Car
+    {
+
+       private string model;
+
+        public string Model { get { return model; } set { model = value; } }
+
+        public string Make { get; set; }
+
+    }
+}
